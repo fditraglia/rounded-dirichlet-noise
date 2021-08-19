12328c4ad392bc43458166db30cd708685c999c3
Francis DiTraglia <francis.ditraglia@economics.ox.ac.uk>
2021-08-19 16:26:17 +0100

    new version with reactive button

--- rounded-dirichlet-noise/app.R
+++ rounded-dirichlet-noise/app.R
@@ -39,9 +39,24 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    sim_raw <- eventReactive(input$draw, { 
+        set.seed(input$seed) 
+        rdirichlet(1, rep(1, input$k)) 
+    })
+    
     output$distPlot <- renderPlot({
-        draw_noise(k = input$k, myseed = input$seed)
+        # To avoid floating point issues, represent rounded probabilities as 
+        # integers between zero and 100 (% points) 
+        sim <- as.integer(round(100 * drop(sim_raw())))
+        # After rounding, probs. may not sum to one. To ensure this, choose a 
+        # coordinate at random and adjust it to satisfy the adding-up constraint
+        adjust_me <- sample(1:length(sim), 1) 
+        sim[adjust_me] <- 100 - sum(sim[-adjust_me])
+        plot(sim, type = 'h', xlab = 'Option #', ylab = 'Percentage (%)',  
+             ylim = c(0, max(sim) * 1.2)) 
+        points(1:length(sim), sim, pch = 20)
+        text(x = 1:length(sim), y = sim + 0.1 * max(sim), 
+             labels = paste0(sim, '%'))
     })
 }
 
