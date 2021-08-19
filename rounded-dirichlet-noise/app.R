b7457cc1256f2ce7e1e6b0efc643323490648e2e
Francis DiTraglia <francis.ditraglia@economics.ox.ac.uk>
2021-08-19 16:03:03 +0100

    initial draft of app

---
+++ rounded-dirichlet-noise/app.R
@@ -0,0 +1,49 @@
+library(shiny)
+
+# The rdirichlet function is taken verbatim from the MCMCpack R package
+rdirichlet <- function(n, alpha) {
+  l <- length(alpha)
+  x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
+  sm <- x %*% rep(1, l)
+  return(x / as.vector(sm))
+}
+
+draw_noise <- function(k, myseed) {
+  set.seed(myseed)
+  sim_raw <- rdirichlet(1, rep(1, k))
+  # To avoid floating point issues, represent rounded probabilities as integers
+  # between zero and 100 (% points)
+  sim <- as.integer(round(100 * drop(sim_raw)))
+  
+  # After rounding, probs. may not sum to one. To ensure this, choose a coordinate
+  # at random and adjust it to satisfy the adding-up constraint
+  adjust_me <- sample(1:k, 1)
+  sim[adjust_me] <- 100 - sum(sim[-adjust_me])
+  
+  plot(1:k, sim, type = 'h', xlab = 'Option #', ylab = 'Percentage (%)', 
+       ylim = c(0, max(sim) * 1.2))
+  points(1:k, sim, pch = 20)
+  text(x = 1:k, y = sim + 0.1 * max(sim), labels = paste0(sim, '%'))
+}
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Rounded Dirichlet Noise"),
+    textInput("seed", "Enter a random seed", value = 675689),
+    numericInput("k", "Dimension of Dirichlet (k)", value = 20, min = 2, max = 30),
+    actionButton("draw", "Draw!", icon = icon("dice")),
+    plotOutput("distPlot")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        draw_noise(k = input$k, myseed = input$seed)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
